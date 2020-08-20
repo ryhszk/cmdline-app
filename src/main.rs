b87d2bc2e8b7ba60094d19b3a142a911eaedccb3
Ryohei Suzuki <ryh.szk.cmnty@gmail.com>
2020-08-20 23:06:08 +0900

    main.rs: Add test code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 /* like the grep */
-/* 
 use structopt::StructOpt;
+use failure::ResultExt;
+use exitfailure::ExitFailure;
+//use std::io::{stdout, Write};
+// use std::fmt::Write;
+// use std::io;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
@@ -12,17 +16,36 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
-    let args = Cli::from_args();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
     for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line);
+            //println!("{}", line);
         }
     }
 }
-*/
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
+}
+
+fn main() -> Result<(), ExitFailure>  {
+    let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|_| format!("could not read file `{}`", args.path.display()))?;
+
+    // for line in content.lines() {
+    //     if line.contains(&args.pattern) {
+    //         println!("{}", line);
+    //     }
+    // }
+    find_matches(&content, &args.pattern, &mut std::io::stdout());
+
+    Ok(())
+}
 
 /* Error Report */
 /*
@@ -54,13 +77,45 @@ fn main() {
 }
 */
 
-use log::{info, warn};
+// use log::{info, warn};
 
-fn main() {
-    //env_logger::init();
-    info!("starting up");
-    warn!("oops, nothing implemented!");
-}
+// fn main() {
+//     //env_logger::init();
+//     info!("starting up");
+//     warn!("oops, nothing implemented!");
+// }
 
 // Let's try it! 
 // $ cargo run -- main src/main.rs
+
+// #[test]
+// fn find_a_match() {
+//     let mut result = Vec::new();
+//     find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+//     assert_eq!(result, b"lorem ipsum\n");
+// }
+
+// fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
+//     for line in content.lines() {
+//         if line.contains(pattern) {
+//             writeln!(writer, "{}", line);
+//         }
+//     }
+// }
+
+// #[test]
+// fn find_a_match() {
+//     let mut result = Vec::new();
+//     find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+//     assert_eq!(result, b"lorem ipsum\n");
+// }
+
+// fn main() -> Result<(), ExitFailure> {
+//     let args = Cli::from_args();
+//     let content = std::fs::read_to_string(&args.path)
+//         .with_context(|_| format!("could not read file `{}`", args.path.display()))?;
+
+//     find_matches(&content, &args.pattern, &mut std::io::stdout());
+
+//     Ok(())
+// }
